b4f9f84ff3d34917dc76a850d4d88da85672e0a0
lolenko <lolenko@finch-melrose.com>
2014-10-25 13:01:17 +0400

    Начнём

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+require.config({
+    baseUrl: "scripts",
+    paths: {
+        jquery: 'vendor/jquery-1.10.2.min'
+    }
+});
+
+require([], function() {
+
+    'use strict';
+
+    alert('Hello World');
+
+});
